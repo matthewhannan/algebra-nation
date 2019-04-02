a0c145b071241064b69cdcda95c08305951c7173
Kyler Clegg <kclegg101@hotmail.com>
2019-04-02 09:46:13 -0400

    Updated .js file to find WOEID (if exists) based onclick

--- client/loadjson.js
+++ client/loadjson.js
@@ -1,6 +1,19 @@
 function loadjson() { 
 	console.log("clicked")
-	var mydata = data[0];
-	console.log(mydata)
-	console.log("parse_good")
+	var mydata = data;
+	//console.log(mydata[0])
+
+	var co = document.getElementById("countryId");
+	var ci = document.getElementById("cityId");
+	var country = co.options[co.selectedIndex].value;
+	var city = ci.options[ci.selectedIndex].value;
+	console.log(country)
+	console.log(city)
+
+	for(var x = 0; x < data.length; x++){
+		if(data[x].name == city && data[x].country == country){
+			console.log("FOUND WOEID")
+			console.log(data[x].woeid)
+		}
+	}
 }
