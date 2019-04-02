2e773327928dc3c73122f7fb3d888212a13d4e1d
Kyler Clegg <kclegg101@hotmail.com>
2019-04-02 10:37:59 -0400

    Updated loadjson

--- client/loadjson.js
+++ client/loadjson.js
@@ -9,11 +9,21 @@ function loadjson() {
 	var city = ci.options[ci.selectedIndex].value;
 	console.log(country)
 	console.log(city)
-
+//	boolean exists = false;
+	//var woeid;
+	
 	for(var x = 0; x < data.length; x++){
 		if(data[x].name == city && data[x].country == country){
 			console.log("FOUND WOEID")
 			console.log(data[x].woeid)
+			return data[x].woeid;
+			//woeid = data[x].woeid;
+			//exists = true;
 		}
 	}
+	
+//	if(!exists){console.log("WOEID NOT FOUND")}
+//	else{
+		//return woeid;
+//	}
 }
