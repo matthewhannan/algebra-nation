a8dca546c4046024c5794672cbcf84024dae27ee
CJO-20 <cjoneill@me.com>
2019-04-05 13:47:32 -0400

    Minor Change #3 4/5/2019
    This is more of a design choice, but can make things possibly easier for us Wednesday.

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,6 @@
+/* Might be a good idea to put twitter API interface code in its own module*/
+var twitter = angular.module('Twitter');
 
 /* register the application and inject all the necessary dependencies */
-var app = angular.module('directoryApp', []);
+/* pretty much the entire project depends on the interface */
+var app = angular.module('directoryApp', ['Twitter']);
