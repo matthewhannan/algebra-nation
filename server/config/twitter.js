4c021bfbae57f471b87c8d396022280694069e59
Alejandro Pineda <42660963+alepineda1997@users.noreply.github.com>
2019-04-10 15:57:03 -0400

    Sorting

--- server/config/twitter.js
+++ server/config/twitter.js
@@ -1,35 +1,43 @@
-require('dotenv').config()
+//require('dotenv').config()
 var Twit = require('twit');
 var config= require('./config');
 var T=new Twit(config);
 
  var params={
-   q:'Gators',
+   q:'Trump',
    count: 5
 
  }
- 
+
 var myparams={
 	id: 2391279 // denver example
 }
-  
+
 T.get('trends/place',myparams)
   .catch(function (err) {
     console.log('caught error', err.stack)
   })
   .then(function (result) {
-    console.log('data', result.data);
+    //console.log('data', result.data);
 })
 
 T.get('search/tweets',params,gotData);
 //T.get('search/tweets', { q: 'UF', count: 5 }, function(err, data, response);
 function gotData(err,data,response){
   var tweets=data.statuses;
+
+  var sortable = tweets.slice(0);
+  sortable.sort(function(a,b){
+  return(a.retweet_count-b.retweet_count);
+  });
   for(var i =0;i<tweets.length;i++){
-    console.log(tweets[i].text);
+    //console.log(data);
+    console.log(sortable[i].text);
+    //console.log(tweets[i].retweet_count)
+    console.log(sortable[i].retweet_count);
   }
 
-  
+
 // var app = angular.module('tweets', []);
 // app.controller('TopicsController', function($scope) {
 //   $scope.query  ;
