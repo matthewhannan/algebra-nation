6de6a7b160a37f6cb128d388de533759f6d19487
Matt Hannan <matthewh2@ufl.edu>
2019-04-17 14:50:07 -0400

    fixed all drop down lists to only include countries and cities that we have woeids for, implemented quicker functions, sped everything up, shortened, this feature is super lit

--- client/loadjson.js
+++ client/loadjson.js
@@ -1,34 +1,3 @@
 function loadjson () { 
-	console.log("clicked")
-	var mydata = data;
-	//console.log(mydata[0])
-
-	var co = document.getElementById("countryId");
-	var ci = document.getElementById("cityId");
-	var country = co.options[co.selectedIndex].value;
-	var city = ci.options[ci.selectedIndex].value;
-	
-
-//	boolean exists = false;
-	//var woeid;
-	
-	for(var x = 0; x < data.length; x++){
-		if(data[x].name == city && data[x].country == country){
-			console.log("FOUND WOEID")
-			console.log(data[x].woeid)
-			return data[x].woeid;
-			//woeid = data[x].woeid;
-			//exists = true;
-		}
-	}
-
-	/*
-	exports.city = city
-	exports.country = country;
-	*/
-	
-//	if(!exists){console.log("WOEID NOT FOUND")}
-//	else{
-		//return woeid;
-//	}
+	return document.getElementById("cityId").value;
 }
