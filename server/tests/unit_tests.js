a21944ec2e29c3524d8b253609c18aff0bf4f2bf
Kyler Clegg <kclegg101@hotmail.com>
2019-04-21 18:51:35 -0400

    Updated mocha tests

--- server/tests/unit_tests.js
+++ server/tests/unit_tests.js
@@ -3,18 +3,23 @@ var should = require('should');
 before(function(done) {
   // Anything needed before unit tests
   // start, do here ...
-}
+  done();
+})
 	
 describe('Saving to database', function() {
 
   this.timeout(10000);
+  
+  // $ mocha unit_tests.js
 
   it('First API call successful', function(done){
-
+	// should.exist(err);
+	// should.not.exist(err);
+	done();
   });
 	
   it('Second API call successful', function(done){
-  
+	done();
   });
   
   
