9acc343715a017f61f06642702409bdb913c65db
Kyler Clegg <kclegg101@hotmail.com>
2019-04-21 18:36:21 -0400

    File for our 10 unit tests added...

---
+++ server/tests/unit_tests.js
@@ -0,0 +1,22 @@
+var should = require('should');
+
+before(function(done) {
+  // Anything needed before unit tests
+  // start, do here ...
+}
+	
+describe('Saving to database', function() {
+
+  this.timeout(10000);
+
+  it('First API call successful', function(done){
+
+  });
+	
+  it('Second API call successful', function(done){
+  
+  });
+  
+  
+  
+});
